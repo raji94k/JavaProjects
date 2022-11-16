33a3dca72c82b8fdb3a8dd2f28ff98d055c2e5bb
raji94k <116812393+raji94k@users.noreply.github.com>
2022-11-16 22:05:15 +0530

    Add files via upload

---
+++ CalculatorUsingScanner.java
@@ -0,0 +1,60 @@
+package task25oct;
+
+import java.util.Scanner;
+
+public class CalculatorUsingScanner {
+
+	public static void main(String[] args) {
+		
+		 char operator;
+		    Double number1, number2, result;
+
+		    // create an object for Scanner class
+		    Scanner input = new Scanner(System.in);
+
+		    //enter operator
+		    System.out.println("Choose an operator: +, -, *, or /");
+		    operator = input.next().charAt(0);
+
+		    // enter numbers
+		    System.out.println("Enter first number");
+		    number1 = input.nextDouble();
+
+		    System.out.println("Enter second number");
+		    number2 = input.nextDouble();
+
+		    switch (operator) {
+
+		      // performs addition between numbers
+		      case '+':
+		        result = number1 + number2;
+		        System.out.println(number1 + " + " + number2 + " = " + result);
+		        break;
+
+		      // performs subtraction between numbers
+		      case '-':
+		        result = number1 - number2;
+		        System.out.println(number1 + " - " + number2 + " = " + result);
+		        break;
+
+		      // performs multiplication between numbers
+		      case '*':
+		        result = number1 * number2;
+		        System.out.println(number1 + " * " + number2 + " = " + result);
+		        break;
+
+		      // performs division between numbers
+		      case '/':
+		        result = number1 / number2;
+		        System.out.println(number1 + " / " + number2 + " = " + result);
+		        break;
+
+		      default:
+		        System.out.println("Invalid operator!");
+		        break;
+		    }
+
+		  }
+	}
+
+
