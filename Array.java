8ff28ceecb5b0380777c7b3cdad266dc19867246
raji94k <116812393+raji94k@users.noreply.github.com>
2022-11-16 21:49:16 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int a[] = new int [1];
+		int b[] = {1,2,3,5,5,8,5,1,7,1};
+		String name = "RAJI";
+	    String address = "PY";
+		
+		System.out.println(a.length);
+		System.out.println(b.length);
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(name. concat(address));
+		System.out.println(address.endsWith(name));
+		
+	}
+
+}
