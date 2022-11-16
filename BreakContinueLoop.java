8f3935e0203ad9378d86abf927ce60782cbff2d5
raji94k <116812393+raji94k@users.noreply.github.com>
2022-11-16 22:06:03 +0530

    Add files via upload

---
+++ BreakContinueLoop.java
@@ -0,0 +1,22 @@
+package whileDoIfLoop; // package
+
+//class
+public class BreakContinueLoop { 
+	
+	
+	//main function (driver)
+	public static void main(String[] args) {
+		
+		for(int i=0; i<20; i++) {
+			
+			if(i == 5)  {
+				
+				//break;
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
